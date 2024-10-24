176dc90ccf0c0f22049d0eee98d97d8caa982df4
Divyansh <divyanshraj0408@gmail.com>
2024-10-24 22:33:43 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>A list of all my major and minor projects</h1>
+      <ul>
+        <li>
+          <a href="#">Project 1</a>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+            corrupti?
+          </p>
+        </li>
+        <li>
+          <a href="#">Project 1</a>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+            corrupti?
+          </p>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
